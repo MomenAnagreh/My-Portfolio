5d7b36400c49fba8e47d93e4ee180c9549c3ca01
Momen Anagreh <momenanagreh@momens-mbp.myfiosgateway.com>
2022-12-12 13:49:12 -0500

    nav-bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProjectsComponent } from './pages/projects/projects.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     TitleComponent,
     ProjectsComponent,
     ContactComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
